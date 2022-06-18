6967fd1c75b9c1073395ee07016308dbbd84371e
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-06-18 05:20:13 +0100

    rearrange image uploads on dishes.js

--- src/dishes.js
+++ src/dishes.js
@@ -1,3 +1,11 @@
+import firstImge from './assets/pexels-dhyamis-kleber-3584317.jpg';
+import secondImage from './assets/pexels-fadime-erbass-11572734.jpg';
+import thirdImage from './assets/pexels-chevanon-photography-312418.jpg';
+import forthImage from './assets/pexels-brett-jordan-825661.jpg';
+import fifthImage from './assets/pexels-tookapic-7782.jpg';
+import sixImage from './assets/pexels-tookapic-9708.jpg';
+
+
 export function dishes() {
     const contentDiv = document.querySelector('#content');
 
@@ -10,7 +18,7 @@ export function dishes() {
     firstDish.classList.add("firstDish");
     const firstImg = document.createElement("img");
     firstImg.classList.add("first-dish-imag");
-    firstImg.src = "../src/assets/pexels-dhyamis-kleber-3584317.jpg";
+    firstImg.src = firstImge;
     firstDish.textContent = "Black Tea as low as \n $2";
     firstImg.alt = "first dish Image";
     
@@ -21,7 +29,7 @@ export function dishes() {
     secondDish.classList.add("firstDish");
     const secondImg = document.createElement("img");
     secondImg.classList.add("first-dish-imag");
-    secondImg.src = "../src/assets/pexels-fadime-erbass-11572734.jpg";
+    secondImg.src = secondImage;
     secondDish.textContent = "Creamed Tea as low as \n $2";
     secondImg.alt = "second dish Image";
     
@@ -32,7 +40,7 @@ export function dishes() {
     thirdDish.classList.add("firstDish");
     const thirdImg = document.createElement("img");
     thirdImg.classList.add("first-dish-imag");
-    thirdImg.src = "../src/assets/pexels-chevanon-photography-312418.jpg";
+    thirdImg.src = thirdImage;
     thirdDish.textContent = "Sun creamed Tea as low as \n $3";
     thirdImg.alt = "third dish Image";
     
@@ -43,7 +51,7 @@ export function dishes() {
     forthDish.classList.add("firstDish");
     const forthImg = document.createElement("img");
     forthImg.classList.add("first-dish-imag");
-    forthImg.src = "../src/assets/pexels-brett-jordan-825661.jpg";
+    forthImg.src = forthImage;
     forthDish.textContent = "Jordan Pizza as low as \n $5";
     forthImg.alt = "forth dish Image";
     
@@ -54,7 +62,7 @@ export function dishes() {
     fifthDish.classList.add("firstDish");
     const fifthImg = document.createElement("img");
     fifthImg.classList.add("first-dish-imag");
-    fifthImg.src = "../src/assets/pexels-tookapic-7782.jpg";
+    fifthImg.src = fifthImage;
     fifthDish.textContent = "Stake with Chips as low as \n $7";
     fifthImg.alt = "fifth dish Image";
     
@@ -65,7 +73,7 @@ export function dishes() {
     sixDish.classList.add("firstDish");
     const sixImg = document.createElement("img");
     sixImg.classList.add("first-dish-imag");
-    sixImg.src = "../src/assets/pexels-tookapic-9708.jpg";
+    sixImg.src = sixImage;
     sixDish.textContent = "Pizza Fruits low as \n $10";
     sixImg.alt = "six dish Image";
     
